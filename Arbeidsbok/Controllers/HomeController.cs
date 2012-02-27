f2ac154ecd2cefd3ce057f545a4b0204890334fc
mariekj <mariek.johansen@gmail.com>
2012-02-27 20:55:27 +0100

    Project added

---
+++ Arbeidsbok/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arbeidsbok.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var message = new []{"gaute", "marie"};
+
+            return View(message);
+        }
+    }
+}
